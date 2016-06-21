750f287abc7787015dea22759fc57ac5db383dbe
卢佳明 <571836353@qq.com>
2016-06-21 10:17:08 +0800

    Initial Commit

---
+++ order2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  order2
+//
+//  Created by 20141105055ljm on 16/6/21.
+//  Copyright © 2016年 20141105055ljm. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
