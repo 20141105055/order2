8e31e34c5d66025ee298c5905465e516f9dbabbb
卢佳明 <571836353@qq.com>
2016-06-21 11:08:56 +0800

    第二版本
    运用冒泡排序实现数据的大小排序，在循环语句中调用输入语句，最后输出。冒泡排序：用两个循环语句实现，外面的循环语句控制总的比
    较次数，内置的循环语句控制比较次数（随比较次数增加而总比较次数减少）将大的值赋值给小的，定义一个中间变量，实现输出

--- order2/main.swift
+++ order2/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
 
+var array =
+    ["1","3","5","4","7","12","53","76","11","45","55","65","2","222","444","555","666","123","23","213"]
+print("个数:\(array.count)")
+print("排序前的顺序:")
+for num in array{
+    print(num)
+}
+
+for var i = 0;i<array.count-1;++i {
+  
+
+for var j = 0;j<array.count-1;++j {
+    
+    
+        if array[i]>array[i+1]
+        {
+            
+            var t=array[i]
+            array[i] = array[i+1]
+            array[i+1]=t
+    }
+    }
+}
+print("排序好的数组：\n")
+
+for num1 in array{
+    print(num1)
+}
